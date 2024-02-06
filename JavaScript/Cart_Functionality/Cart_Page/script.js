ddf56fe4bbb09239d97949e68187286d51a8bbf8
tushar <tushargadher123@gmail.com>
2024-02-06 13:17:02 +0530

    cart functionality complete

--- JavaScript/Cart_Functionality/Cart_Page/script.js
+++ JavaScript/Cart_Functionality/Cart_Page/script.js
@@ -1,32 +1,90 @@
 // alert("Cart Page");
-let productData = JSON.parse(localStorage.getItem("productData")) || [];
-productData.forEach((product) => {
-  let tbody = document.getElementsByTagName("tbody")[0];
-  const row = document.createElement("tr");
-  row.innerHTML = `
-      <td>${product.name}</td>
-      <td>${product.quantity}</td>
-      <td>$${product.price}</td>
-    `;
-  tbody.appendChild(row);
+document.addEventListener("DOMContentLoaded", () => {
+  let productData = [
+    {
+      id: 1,
+      name: "Strandmond",
+      price: 295.63,
+      quantity: 0,
+      image: "../asset/images/sofa.jpg",
+    },
+    {
+      id: 2,
+      name: "Mellby",
+      price: 749.62,
+      quantity: 0,
+      image: "../asset/images/mellby.jpg",
+    },
+    {
+      id: 3,
+      name: "Micke",
+      price: 144.21,
+      quantity: 0,
+      image: "../asset/images/micke.jpg",
+    },
+    {
+      id: 4,
+      name: "Sheesham Queen Size Bed",
+      price: 437.54,
+      quantity: 0,
+      image: "../asset/images/bad.jpg",
+    },
+  ];
+  RenderProduct();
+  calculateFair();
+  let addProductButton = document.getElementById("addProductButton");
+  let placeorderButton = document.getElementById("placeorderButton");
+
+  addProductButton.addEventListener("click", () => {
+    let conformed = confirm("You Will Be Redirect to Product Page?");
+    conformed
+      ? (window.location.href = "http://127.0.0.1:5501/Product_Page/")
+      : "";
+  });
+
+  placeorderButton.addEventListener("click", placeorder);
+  function RenderProduct() {
+    let productData = JSON.parse(localStorage.getItem("productData")) || [];
+    const table = document.querySelector("table");
+    const tbody = document.querySelector("tbody");
+    const cartEmptyRow = `<h2>Your Cart Is Empty. Please Add Some Products.</h2>`;
+    let isCartEmpty = true;
+
+    productData.forEach((product) => {
+      const row = document.createElement("tr");
+      if (product.quantity) {
+        isCartEmpty = false;
+        row.innerHTML = `
+            <td>${product.name}</td>
+            <td>${product.quantity}</td>
+            <td>$${product.price}</td>
+          `;
+        tbody.appendChild(row);
+      }
+    });
+    table.innerHTML = isCartEmpty ? cartEmptyRow : table.innerHTML;
+  }
+  function calculateFair() {
+    let productData = JSON.parse(localStorage.getItem("productData")) || [];
+
+    let totalAmount = productData.reduce((total, product) => {
+      return total + product.price * product.quantity;
+    }, 0);
+
+    let TotalWithGST = totalAmount * 0.18 + totalAmount;
+    let str = TotalWithGST.toString(); //convert number to string
+    let result = str.substring(0, 7); //first six character
+    result = parseFloat(result); // convert it to a number
+    let totalDue = document.getElementById("totalDue");
+    totalDue ? (totalDue.innerHTML = `<b>$${result}</b>`) : "";
+  }
+
+  function placeorder() {
+    let conformed = confirm("Place Order ?");
+    if (conformed) {
+      alert("Your Order is placed");
+      localStorage.setItem("productData", JSON.stringify(productData));
+      window.location.href = "http://127.0.0.1:5501/Product_Page/";
+    }
+  }
 });
-
-
-
-let total = 0;
-productData.map((product) => {
-  productPrice = Number.parseFloat(product.price) * product.quantity;
-  console.log(total);
-  total = productPrice + total;
-});
-
-let TotalWithGST = total * 0.18 + total;
-
-var str = TotalWithGST.toString(); //convert number to string
-var result = str.substring(0, 7); // cut six first character
-result = parseFloat(result); // convert it to a number
-document.getElementById("totalDue").innerHTML = `<b>$${result}</b>`;
-console.log(total);
-
-
-// productData.reducer
--- JavaScript/Cart_Functionality/Product_Page/script.js
+++ JavaScript/Cart_Functionality/Product_Page/script.js
@@ -4,34 +4,47 @@ if (!productData.length) {
     {
       id: 1,
       name: "Strandmond",
-      price: "295.63",
+      price: 295.63,
       quantity: 0,
       image: "../asset/images/sofa.jpg",
     },
     {
       id: 2,
       name: "Mellby",
-      price: "749.62",
+      price: 749.62,
       quantity: 0,
       image: "../asset/images/mellby.jpg",
     },
     {
       id: 3,
       name: "Micke",
-      price: "144.21",
+      price: 144.21,
       quantity: 0,
       image: "../asset/images/micke.jpg",
+    },
+    {
+      id: 4,
+      name: "Sheesham Queen Size Bed",
+      price: 437.54,
+      quantity: 0,
+      image: "../asset/images/bad.jpg",
     },
   ];
   localStorage.setItem("productData", JSON.stringify(productData));
 }
-
 RenderProduct();
+function calculateBill() {
+  let productData = JSON.parse(localStorage.getItem("productData")) || [];
+  const totalAmount = productData.reduce((total, product) => {
+    return total + product.price * product.quantity;
+  }, 0);
+  return totalAmount;
+}
+
 //function to increment quanlity of product
 function incrementQuantity(id) {
   let productData = JSON.parse(localStorage.getItem("productData")) || [];
   let productIndex = productData.findIndex((product) => product.id === id);
-  //   alert(productIndex);
   console.log(productData[productIndex]);
   productData[productIndex] = {
     ...productData[productIndex],
@@ -39,43 +52,50 @@ function incrementQuantity(id) {
   };
   console.log(productData[productIndex]);
   localStorage.setItem("productData", JSON.stringify(productData));
+  RenderProduct();
 }
 
 //functiont to descement quanlity of product
 function decrementQuantity(id) {
   let productData = JSON.parse(localStorage.getItem("productData")) || [];
   let productIndex = productData.findIndex((product) => product.id === id);
-  productData[productIndex] = {
-    ...productData[productIndex],
-    quantity: productData[productIndex].quantity - 1,
-  };
-  console.log(productData[productIndex]);
-  localStorage.setItem("productData", JSON.stringify(productData));
+  if (productData[productIndex].quantity <= 0) {
+    alert("Product Quantity can't be negative");
+  } else {
+    productData[productIndex] = {
+      ...productData[productIndex],
+      quantity: productData[productIndex].quantity - 1,
+    };
+    console.log(productData[productIndex]);
+    localStorage.setItem("productData", JSON.stringify(productData));
+    RenderProduct();
+  }
 }
 
 //render product page
 function RenderProduct() {
   let productData = JSON.parse(localStorage.getItem("productData")) || [];
-  let productContainer = document.getElementsByClassName("productContainer")[0];
-
+  const productContainer = document.querySelector(".productContainer");
+  productContainer.innerHTML = "";
   productData.forEach((item) => {
     let productDiv = document.createElement("div");
     productDiv.setAttribute("class", "product");
     productDiv.innerHTML = `
-        <div class="product-details">
-           <img src=${item.image} alt=${item.name} srcset="" />
-           <div class="price">
-              <span>${item.name}</span>
-              <span>$${item.price}</span>
-           </div>
-        </div>
-        <div class="buttonGroup">
-            <button onClick="decrementQuantity(${item.id})"> - </button>
-            <span>${item.quantity}</span>
-            <button onClick="incrementQuantity(${item.id})"> + </button>
-        </div>`;
+          <div class="product-details">
+             <img src=${item.image} alt=${item.name} srcset="" />
+             <div class="price">
+                <span>${item.name}</span>
+                <span>$${item.price}</span>
+             </div>
+          </div>
+          <div class="buttonGroup">
+              <button onClick="decrementQuantity(${item.id})"> - </button>
+              <span>${item.quantity}</span>
+              <button onClick="incrementQuantity(${item.id})"> + </button>
+          </div>`;
     productContainer.appendChild(productDiv);
   });
+  document.getElementById("totalAmount").innerHTML = `$${calculateBill()}`;
 }
 
 //function checkout
